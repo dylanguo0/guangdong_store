665153bb9d9c0158fecf488838e8a6515108cc4d
dylanguo0 <dylanguo@riccarton.school.nz>
2026-05-13 10:09:51 +1200

    Created the nav bar in layout.html

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__,
-)
+            template_folder='website/templates', 
+            static_folder='website/static')
 
 @app.route('/')
 def home():
