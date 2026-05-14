1ca0955524db758826b8581a518a512947f89185
dylanguo0 <dylanguo@riccarton.school.nz>
2026-05-15 11:53:37 +1200

    Fixed the nav bar not working and adding the extends layout.html to every page

--- main.py
+++ main.py
@@ -8,5 +8,21 @@ app = Flask(__name__,
 def home():
     return render_template('store.html')
 
+@app.route('/store')
+def store():
+    return render_template('store.html')
+
+@app.route('/wishlist')
+def wishlist():
+    return render_template('wishlist.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
