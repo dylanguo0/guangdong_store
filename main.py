0f0a600038ef486c048c0d79b4771d337beb93ba
dylanguo0 <dylanguo@riccarton.school.nz>
2026-05-15 12:01:12 +1200

    Added the app routes for login and signup

--- main.py
+++ main.py
@@ -24,5 +24,13 @@ def checkout():
 def profile():
     return render_template('profile.html')
 
+@app.route('/login')
+def profile():
+    return render_template('login.html')
+
+@app.route('/signup')
+def profile():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
